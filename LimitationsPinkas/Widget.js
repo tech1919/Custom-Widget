0019945653a118e0bbf4fa28e142b6232cb35adc
Roi Levi <74343893+RoiLe@users.noreply.github.com>
2022-12-28 10:09:51 +0200

    Update Widget.js

--- LimitationsPinkas/Widget.js
+++ LimitationsPinkas/Widget.js
@@ -6,33 +6,60 @@ function(declare, BaseWidget) {
 
     startup: function(){
       var sceneElements = document.getElementsByClassName("esri-view-root");
+      var zoomInButton = document.getElementsByClassName("esri-icon esri-icon-plus");
+
+      zoomInButton[1].addEventListener('click',  (event) => {
+        this._zoomLimited()
+       });
 
       sceneElements[0].addEventListener('mousewheel', (event) => {
         this._zoomLimited()
       });
+
+      sceneElements[0].addEventListener('dblclick', (event) => {
+        this._zoomLimited()
+      });
+
       sceneElements[0].addEventListener('click', (event) => {
         this._navigateLimited()
       });   
-      
     },  
     _zoomLimited: function(){
-      var currentZoom = Number(this.sceneView.zoom).toFixed(4);
-      var limitZoomValue = 19.33;
-      
-      if(currentZoom > limitZoomValue){
-        this.sceneView.zoom = limitZoomValue;
+      var currentZoom = Number(this.sceneView.zoom);
+      var limitZoomValue = 20.5;
+      var eventType = event.type;
+      var limitFlag = 0; 
+
+      switch(eventType){
+        case  'click':
+          limitFlag = 0;
+          if(currentZoom > limitZoomValue){this.sceneView.zoom -= 1;}
+          break;
+        case 'dblclick':
+          limitFlag = (event.deltaY == null)? 1 : 0 ;
+          break;
+        case 'mousewheel':
+          limitFlag = (event.deltaY < 0)? 1 : 0 ;
+          break;
+      }
+
+      if(currentZoom > limitZoomValue && limitFlag){
+        this.sceneView.zoom = currentZoom;
       }
       
     },
     _navigateLimited: function(){
-      var limitTiltValue = 55;
+      var limitTiltValue = 65;
       var currentTilt = this.sceneView.camera.tilt;
       var newCam = this.sceneView.camera.clone();
+      var positionX = this.sceneView.camera.position.x;
+      var positionY = this.sceneView.camera.position.y;
       newCam.tilt = limitTiltValue;
-      
+      newCam.position.x = positionX;
+      newCam.position.y = positionY;
 
-      if(currentTilt > limitTiltValue){
-          this.sceneView.camera = newCam;
+      if(currentTilt > limitTiltValue){  
+        this.sceneView.camera = newCam;
       }
     },
       
